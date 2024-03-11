e495b69a2439c5bb171a04eec1e6339c93b49470
Igor Volchenok <checklogon@gmail.com>
2024-03-11 14:02:35 +0700

    chore: comment out the code related to web workers

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
 'use strict';
 import { day } from "./utils/currentDay";
 
-const sharedBuffer = new SharedArrayBuffer(4); // Размер достаточный для одного Int32
-const sharedArray = new Int32Array(sharedBuffer);
+// const sharedBuffer = new SharedArrayBuffer(4); // Размер достаточный для одного Int32
+// const sharedArray = new Int32Array(sharedBuffer);
 
-const worker = new Worker('worker.js', { type: 'module' });
-worker.postMessage({ sharedBuffer });
+// const worker = new Worker('worker.js');
+// worker.postMessage({ sharedBuffer });
 
-const countEl = document.getElementById('count');
+// const countEl = document.getElementById('count');
 
-// Слушаем сообщения от worker для обновления счётчика
-worker.onmessage = function(e) {
-    countEl.textContent = e.data;
-};
+// // Слушаем сообщения от worker для обновления счётчика
+// worker.onmessage = function(e) {
+//     countEl.textContent = e.data;
+// };
 
-document.getElementById('visitButton').addEventListener('click', function() {
-    worker.postMessage('visit');
-});
+// document.getElementById('visitButton').addEventListener('click', function() {
+//     worker.postMessage('visit');
+// });
 
 
 // Service Worker registration
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', { type: 'module' }).then(function(registration) {
+    navigator.serviceWorker.register('service-worker.js', {}).then(function(registration) {
       console.log('Service Worker registered with scope: ', registration.scope);
     }).catch(function(err) {
       console.log('Service Worker registration failed: ', err);
     });
 }
 
-const URL = `https://firestore.googleapis.com/v1/projects/numod-20528/databases/(default)/documents/nums/${day}`;
-const AUTH_TOKEN = '';
+const URL =`https://numod-20528-default-rtdb.europe-west1.firebasedatabase.app/nums/${day}.json`;
 
 // Get data from the server
 fetch(URL,{
     method: 'GET',
     headers: {
-        'Authorization': `Bearer ${AUTH_TOKEN}`,
         'Content-Type': 'application/json',
     }
     })
@@ -47,8 +45,7 @@ fetch(URL,{
   })
   .then(data => {
     const container = document.getElementById('data-container');
-    const nod = data.fields.value.integerValue;
-    container.textContent = nod;
+    container.textContent = data.value;
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
