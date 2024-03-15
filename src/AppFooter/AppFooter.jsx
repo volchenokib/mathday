d2f2bd7432fc9ac3ee5dab0915ca583ec3cca249
Igor Volchenok <checklogon@gmail.com>
2024-03-16 02:12:55 +0700

    style: Update footer styling and add small font class

--- src/AppFooter/AppFooter.jsx
+++ src/AppFooter/AppFooter.jsx
@@ -4,8 +4,8 @@ import "./appFooter.css"
 function AppFooter() {
   return (
     <footer className="app-footer">
-      <div className="footer-inner">
-        <span className="text-center">
+      <div>
+        <span className="small" style={{ color: "#fafafa" }}>
           © Игорь Волченок, {new Date().getFullYear()}
         </span>
       </div>
