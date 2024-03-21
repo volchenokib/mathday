23af65fafc7bb507716c62f8227846013467e58d
Igor Volchenok <checklogon@gmail.com>
2024-03-21 23:32:14 +0700

    style: add the site logo

--- src/AppHeader/AppHeader.jsx
+++ src/AppHeader/AppHeader.jsx
@@ -5,7 +5,11 @@ import NumLang from "../stories/NumLang/NumLang.jsx"
 function AppHeader() {
   return (
     <header className={"app-header"}>
-      <span className={"siteName"}>Math Day</span>
+      <div className="app-logo-container">
+        <img src="./logo.svg" alt="site logo" />
+        <span className={"siteName"}>Math Day</span>
+      </div>
+
       <NumLang />
     </header>
   )
