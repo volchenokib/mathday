4a8bd63ef60ecbbffb36a3b87cb35150a65e0c0f
Igor Volchenok <checklogon@gmail.com>
2024-03-21 15:51:56 +0700

    chore: Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import fs from "fs"
-import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Устанавливаем базовый путь как относительный
-  server: {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    },
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "./localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "./localhost.pem")),
-    },
-  },
+  base: "./", // Set the base path for the project
 })
