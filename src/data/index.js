9359cec426f9b262af8ea0dbf152c280fac74cdd
Igor Volchenok <checklogon@gmail.com>
2024-03-21 22:04:56 +0700

    chore: clean up

--- src/data/index.js
+++ src/data/index.js
@@ -1,16 +1,5 @@
-import { sequences } from "./sequenceList.js"
 import { nodList } from "./nodList.js"
 import { day } from "../utils/index.js"
-
-function findSequencesName(value) {
-  let sequencesName = []
-  sequences.forEach((v, k) => {
-    if (v.includes(value)) {
-      sequencesName.push(k)
-    }
-  })
-  return sequencesName
-}
 
 const id = day - 1
 const value = nodList[id]
@@ -19,7 +8,6 @@ export const nodData = {
   id,
   value,
   content: {
-    sequences: findSequencesName(value),
     infoRu: [],
   },
 }
