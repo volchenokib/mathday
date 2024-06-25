ae91282e1ede961e32465a137137de6bf9fdcd81
Igor Volchenok <checklogon@gmail.com>
2024-06-25 21:47:51 +0700

    chore: Update Node.js version requirement to include 21.7.3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   base: "./", // Set the base path for the project
+  server: {
+    port: 5174, // Set the port number
+    // open: true, // Open the browser automatically
+    // cors: true, // Allow CORS
+  },
 })
